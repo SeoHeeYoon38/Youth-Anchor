efc923dceea05656f1f5731e428e547cc85a9df5
YoonSeoHee <helen38sh@gmail.com>
2026-09-22 01:55:29 +0900

    fix: update Anyang location placeholder

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // 위치 권한을 허용하지 않아도 안양시 쉼터 3곳의 중심에서 지도가 열린다.
 export const DEFAULT_CENTER = { lat: 37.3918153, lng: 126.9466077 }
-export const DEFAULT_LOCATION_MESSAGE = '안양시 쉼터 밀집 지역을 기준으로 보여드려요'
+export const DEFAULT_LOCATION_MESSAGE = '안양시청기준으로 보여드려요'
